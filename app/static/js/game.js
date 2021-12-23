b89e59ed0cabd67c0db2c9262e8f869ec979b0a9
Appa <appa.romanova@gmail.com>
2021-12-23 21:47:19 +0300

    +game page

---
+++ app/static/js/game.js
@@ -0,0 +1,4 @@
+document.getElementById("first").style.visibility = 'visible'
+document.getElementById("gift-given").style.visibility = 'visible'
+document.getElementById("second-section").style.visibility = 'hidden'
+document.getElementById("final").style.visibility = 'hidden'
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -14,5 +14,3 @@ document.getElementById("show-login").onclick = function() {
   document.getElementById("login-section").style.visibility = 'visible'
   document.getElementById("register-section").style.visibility = 'hidden'  
 }
-
-document.getElementById("game-list").style.visibility = 'hidden'
