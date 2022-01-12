ff7de3de919244729a50bc3a79146d5c26ea5636
yanychek <yandan01@gmail.com>
2022-01-12 14:33:43 +0300

    1 chek

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,33 @@
 import flask
+from flask import session
 from flask import request
 from app import app
 from app.db_interaction import log_user
 from app.db_interaction import add_user
 
 
+
+
 @app.route('/')
-#@app.route('/index')
 @app.route('/home', methods=["GET", "POST"])
 def home():
     return flask.render_template('index.html')
 
-@app.route('/login', methods=["GET", "POST"])
-def login():
-    return flask.render_template('login.html')
+
+# @app.route('/login', methods=["GET", "POST"])
+# def login():
+#     return flask.render_template('login.html')
+
 
 @app.route('/user', methods=["POST"])
 def user():
     login, passwd = request.form['name'], request.form['password']
     if log_user(login, passwd):
+        session["username"] = login
         return flask.render_template('user.html')
     else:
         return "Wrong login or password"
+
 
 @app.route('/register', methods=["POST"])
 def register():
@@ -29,10 +35,18 @@ def register():
     add_user(login, passwd)
     return "Welcome " + login + "!"
 
+
 @app.route('/creategame', methods=["POST"])
 def creategame():
     return request.form['name']
 
+
 @app.route('/game', methods=["GET", "POST"])
 def game():
     return flask.render_template('game.html')
+
+
+@app.route('/logout', methods=["GET", "POST"])
+def logout():
+    session.pop("username", None)
+    return flask.render_template('index.html')
