d420923d380fec1ebcdaa07cd149450ab2b33d4c
yanychek <yandan01@gmail.com>
2022-01-12 16:02:18 +0300

    Sessins setup and some twiks with register and username in LK

--- app/routes.py
+++ app/routes.py
@@ -5,35 +5,42 @@ from app import app
 from app.db_interaction import log_user
 from app.db_interaction import add_user
 
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 @app.route('/')
 @app.route('/home', methods=["GET", "POST"])
 def home():
-    return flask.render_template('index.html')
+    try:
+        if session['username'] != None:
+            return flask.render_template('index_logedin.html')
+    except KeyError:
+        return flask.render_template('index.html')
 
 
-# @app.route('/login', methods=["GET", "POST"])
-# def login():
-#     return flask.render_template('login.html')
-
-
-@app.route('/user', methods=["POST"])
+@app.route('/user', methods=["GET", "POST"])
 def user():
-    login, passwd = request.form['name'], request.form['password']
-    if log_user(login, passwd):
-        session["username"] = login
-        return flask.render_template('user.html')
-    else:
-        return "Wrong login or password"
+    if request.method == "GET":
+        try:
+             if session['username'] != None:
+                 data = [session['username']]
+                 return flask.render_template('user.html', data=data)
+        except KeyError:
+            return "Please log in"
+    elif request.method == "POST":
+        login, passwd = request.form['name'], request.form['password']
+        if log_user(login, passwd):
+            session["username"] = login
+            return flask.render_template('user.html')
+        else:
+            return "Wrong login or password"
 
 
 @app.route('/register', methods=["POST"])
 def register():
     login, passwd = request.form['name'], request.form['password']
     add_user(login, passwd)
-    return "Welcome " + login + "!"
+    return user()
 
 
 @app.route('/creategame', methods=["POST"])
