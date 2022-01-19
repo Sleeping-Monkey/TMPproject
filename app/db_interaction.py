d957de45f1e6d124de3bd6ebbc2a1d751891347d
yanychek <yandan01@gmail.com>
2022-01-19 17:07:08 +0300

    return of player_id and mmr for present distribution function

--- app/db_interaction.py
+++ app/db_interaction.py
@@ -4,8 +4,8 @@ import sqlite3
 def add_user(login, passwd):
     conn = sqlite3.connect("database.db")
     curs = conn.cursor()
-    sql_req = """INSERT INTO User (Login, Password) VALUES (?, ?)"""
-    data = (login, passwd)
+    sql_req = """INSERT INTO User (Login, Password, Mmr) VALUES (?, ?, ?)"""
+    data = (login, passwd, 1000)
     curs.execute(sql_req, data)
     conn.commit()
     conn.close()
@@ -28,7 +28,7 @@ def create_game(game_name, player_limit, creator_name):
     conn = sqlite3.connect("database.db")
     curs = conn.cursor()
     sql_req = """INSERT INTO game_info (game_name, creator_id, player_limit) VALUES (?,?,?)"""
-    data = (game_name, id_by_name(creator_name), player_limit)
+    data = (game_name, get_from_db_one_elem(creator_name), player_limit)
     curs.execute(sql_req, data)
     conn.commit()
     conn.close()
@@ -38,18 +38,71 @@ def connect_game(game_name, name):
     conn = sqlite3.connect("database.db")
     curs = conn.cursor()
     sql_req = """INSERT INTO player_list (game_name, player_id) VALUES (?,?)"""
-    data = (game_name, id_by_name(name))
+    data = (game_name, get_from_db_one_elem(name),)
     curs.execute(sql_req, data)
     conn.commit()
     conn.close()
 
 
-def id_by_name(name):
+# def id_by_name(name):
+#     conn = sqlite3.connect("database.db")
+#     curs = conn.cursor()
+#     what_to_return = "ID"
+#     table_name = "User"
+#     column = "login"
+#     sql_req = """SELECT """ + what_to_return + """ FROM """ + table_name + """ WHERE """ + column + """ = ?"""
+#     data = (name,)
+#     id = (curs.execute(sql_req, data)).fetchall()
+#     conn.close()
+#     return id[0][0]
+
+
+def get_from_db_one_elem(search_by_what, what_to_return="ID", table_name="User", column="login", a="0"):
     conn = sqlite3.connect("database.db")
     curs = conn.cursor()
-    sql_req = """SELECT ID FROM User WHERE login = ?"""
-    data = (name,)
-    id = (curs.execute(sql_req, data)).fetchall()
+    sql_req = """SELECT """ + what_to_return + """ FROM """ + table_name + """ WHERE """ + column + """ = ?"""
+    data = (search_by_what,)
+    if a == "-1":
+        name = get_from_db_one_elem(search_by_what)
+        data = (name,)
+    data = (curs.execute(sql_req, data)).fetchall()
     conn.close()
-    return id[0][0]
+    return data[0][0]
+
+
+def is_space_to_connect(game_name):
+    conn = sqlite3.connect("database.db")
+    curs = conn.cursor()
+    sql_req = """SELECT COUNT(player_id) FROM player_list WHERE game_name = ?"""
+    data = (game_name,)
+    data = (curs.execute(sql_req, data)).fetchall()
+    lim = get_from_db_one_elem(game_name, "player_limit", "game_info", "game_name")
+    conn.close()
+    return lim-data[0][0]
+
+
+def connected(player_id, game_name):
+    conn = sqlite3.connect("database.db")
+    curs = conn.cursor()
+    sql_req = """SELECT * FROM player_list WHERE game_name = ? AND player_id = ?"""
+    data = (game_name, get_from_db_one_elem(player_id),)
+    if ((curs.execute(sql_req, data)).fetchone()) is None:
+        return 0
+    else:
+        return 1
+
+
+def presents(game_name="bebra"):
+    conn = sqlite3.connect("database.db")
+    curs = conn.cursor()
+    sql_req = """SELECT User.ID, User.Mmr
+        FROM player_list JOIN User ON User.ID == player_list.player_id AND player_list.game_name == ?"""
+    data = (game_name,)
+    data = (curs.execute(sql_req, data)).fetchall()
+    conn.close()
+    print(data)
+#     return data
+
+
+
 
--- app/routes.py
+++ app/routes.py
@@ -6,13 +6,17 @@ from app.db_interaction import log_user
 from app.db_interaction import add_user
 from app.db_interaction import create_game
 from app.db_interaction import connect_game
-
+from app.db_interaction import get_from_db_one_elem
+from app.db_interaction import is_space_to_connect
+from app.db_interaction import presents
+from app.db_interaction import connected
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 @app.route('/')
 @app.route('/home', methods=["GET", "POST"])
 def home():
+    presents()
     # try:
     #     if session['username'] != None:
     #         session["username"] = ""
@@ -28,8 +32,7 @@ def user():
     if request.method == "GET":
         try:
              if session['username'] != None:
-                 data = [session['username']]
-                 return flask.render_template('user.html', data=data)
+                 return flask.render_template('user.html')
         except KeyError:
             return "Please log in"
     elif request.method == "POST":
@@ -52,18 +55,36 @@ def register():
 def creategame():
     game_name, player_count, creator_name = request.form['name'], request.form['count'], session['username']
     create_game(game_name, player_count, creator_name)
-    return request.form['name']
+    connect_game(game_name, session['username'])
+    # name = get_from_db_one_elem(session['username'], "game_name", "player_list", "player_id", "-1")
+    data = (game_name,)
+    # данные для отображения и разделения на этапы
+    return flask.render_template('game.html', data=data)
+    # return request.form['name']
 
 
 @app.route('/game', methods=["GET", "POST"])
 def game():
-    connect_game(request.form['gameid'], session['username'])
-    data = ()
-    # данные для отображения и разделения на этапы
-    return flask.render_template('game.html')
+    if connected(session['username'], request.form['gameid']):
+        name = get_from_db_one_elem(session['username'], "game_name", "player_list", "player_id", "-1")
+        data = (name,)
+        # данные для отображения и разделения на этапы
+        return flask.render_template('game.html', data=data)
+    elif is_space_to_connect(request.form['gameid']):
+        connect_game(request.form['gameid'], session['username'])
+        name = get_from_db_one_elem(session['username'], "game_name", "player_list", "player_id", "-1")
+        data = (name,)
+        # данные для отображения и разделения на этапы
+        return flask.render_template('game.html', data=data)
+    else:
+        data = 0
+        return flask.render_template('user.html', data=data)
 
 
 @app.route('/logout', methods=["GET", "POST"])
 def logout():
     session.pop("username", None)
     return flask.render_template('index.html')
+
+
+# @app.route('discon')
